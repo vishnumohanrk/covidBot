2d734f7d494446c4b4fd6b768019342c8d92c4d1
Vishnu <mohanvish1999@gmail.com>
2020-04-28 10:54:43 +0530

    major

--- src/app.js
+++ src/app.js
@@ -6,19 +6,22 @@ const twiml = require('twilio').twiml.MessagingResponse
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
+
 const port = process.env.PORT || 3000
 
 app.post('/whatsApp', (req,res) => {
-    console.log(req.body)
-    const msg = new twiml
-    msg.message('Hello')
-    res.end(msg.toString())
-    // setInterval(() => res.write(
-    //     `Remember To Wash Hands,
-    //     Always Wear Masks While going Out
-    //     Social Distancing,
-    //     `
-    // ), 1000)
+    
 })
+
+setInterval(() => {
+    arr.forEach(i => {
+        twClient.messages.create({
+            body: 'Remember To Wash Hands,\nAlways Wear Masks While going Out\nSocial Distancing',
+            from: 'whatsapp:+14155238886',
+            to: i
+        }).then(res => console.log(`Posted - ${i}`))
+        .catch(e => console.log(`Post Error - ${i}`))
+    })
+}, 120000)
 
 app.listen(port, () => console.log(`Running on PORT:${port}`))
