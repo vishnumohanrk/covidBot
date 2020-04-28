59b8d35da7ce77eed000847544893445d5d036c4
Vishnu <mohanvish1999@gmail.com>
2020-04-28 15:13:20 +0530

    whatsapp

--- src/app.js
+++ src/app.js
@@ -6,24 +6,39 @@ const twiml = require('twilio').twiml.MessagingResponse
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
-
 const port = process.env.PORT || 3000
 
+const getOpted = () => {
+    return new Promise((resolve,reject) => {
+        twClient.messages.list({
+            to: 'whatsapp:+14155238886',
+        })
+        .then(res => resolve([... new Set(res.filter(i => i.body.match(/join/gi)).map(i => i.from))]))
+        .catch(e => reject(e))
+    })
+}
+
 app.post('/whatsApp', (req,res) => {
+    console.log(req.body)
     const msg = new twiml
     msg.message('You Opted For Remainders. Reply Stop to Opt out.')
-    return res.send(msg.toString())
+    res.send(msg.toString())
 })
 
 setInterval(() => {
-    arr.forEach(i => {
-        twClient.messages.create({
-            body: 'Remember To Wash Hands,\nAlways Wear Masks While going Out\nSocial Distancing',
-            from: 'whatsapp:+14155238886',
-            to: i
-        }).then(res => console.log(`Posted - ${i}`))
-        .catch(e => console.log(`Post Error - ${i}`))
+    getOpted()
+    .then(res => {
+        console.log(res)
+        res.forEach(i => {
+            twClient.messages.create({
+                body: 'Wash Hands\nSTAY Home\nWear Mask\nKEEP a safe distance\nCOVER your cough\nReply stop to Opt Out',
+                from: 'whatsapp:+14155238886',
+                to: i
+            }).then(res => console.log(`Posted - ${i}`))
+            .catch(e => console.log(`Post Error - ${i}`))
+        })
     })
-}, 120000)
+    .catch(e => console.log('Error --Get Opted'))
+}, 5000)
 
 app.listen(port, () => console.log(`Running on PORT:${port}`))
